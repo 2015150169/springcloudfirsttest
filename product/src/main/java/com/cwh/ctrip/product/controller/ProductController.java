d78cb41e9c950e0c7011561dc8fd71681965a0d0
cwh <513013786@qq.com>
2019-12-21 16:54:21 +0800

    springcloud

---
+++ product/src/main/java/com/cwh/ctrip/product/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.cwh.ctrip.product.controller;
+
+
+
+public class ProductController {
+
+    /**
+     * 1. 查询所有在架的商品
+     * 2. 获取类目type列表
+     * 3. 查询类目
+     * 4. 构造数据
+     */
+
+
+}
