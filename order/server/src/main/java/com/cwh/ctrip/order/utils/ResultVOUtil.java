40eb94ef70801ebc7cc46e7bd51ca1cefca3000c
cwh <513013786@qq.com>
2020-01-01 15:03:59 +0800

    config server

--- order/server/src/main/java/com/cwh/ctrip/order/utils/ResultVOUtil.java
+++ order/server/src/main/java/com/cwh/ctrip/order/utils/ResultVOUtil.java
@@ -11,7 +11,7 @@ public class ResultVOUtil {
     public static ResultVO success(Object object) {
         ResultVO resultVO = new ResultVO();
         resultVO.setCode(0);
-        resultVO.setMsg("成功");
+        resultVO.setMsg("success");
         resultVO.setData(object);
         return resultVO;
     }
